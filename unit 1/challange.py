6b99a923bee0d9b01c006a0be32ea4274dd1c5d6
vishali2922128044 <147617682+vishali2922128044@users.noreply.github.com>
2023-10-11 19:05:59 +0530

    Add files via upload

---
+++ unit 1/challange.py
@@ -0,0 +1,13 @@
+# leap year
+def isleapyear(year):
+  if(year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+    return True
+  else:
+    return false
+
+year = int(input("enter a year :"))
+
+if isleapyear(year):
+  print("{} is a leap year.".format(year))
+else:
+  print("{} is not a leap year.".format(year))
